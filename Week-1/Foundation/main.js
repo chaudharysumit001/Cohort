b614ba26a1d7ce4e0d717de1a417a06953f97991
Pranay Gaikwad <pranaygaikwad1432@gmail.com>
2023-12-06 11:01:25 +0530

    DataTypes

--- Week-1/Foundation/main.js
+++ Week-1/Foundation/main.js
@@ -2,21 +2,34 @@
 
 // console.log(a);
 
-// Variables -- value can change throughout the program
+// // Variables -- value can change throughout the program
 
-//1. let
-var b = 10;
-b = 2;
-console.log(b);
+// //1. let
+// var b = 10;
+// b = 2;
+// console.log(b);
 
-//2. var
-var a = 10;
-a = 2;
-console.log(a);
+// //2. var
+// var a = 10;
+// a = 2;
+// console.log(a);
 
-//3. const
-const c = 10;
-//you cannot change the value
-// c = 2;
-//TypeError: Assignment to constant variable.
-console.log(c);
+// //3. const
+// const c = 10;
+// //you cannot change the value
+// // c = 2;
+// //TypeError: Assignment to constant variable.
+// console.log(c);
+
+//Datatypes
+
+//String
+let firstName = "Pranay";
+
+//Number
+let age = 24;
+
+//Boolean
+let isMarried = false;
+
+console.log("This Person name is " + firstName + " and their age is " + age);
