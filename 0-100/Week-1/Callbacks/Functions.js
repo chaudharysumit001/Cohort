bf91014d15f8ab99c9e26de7e18f8e5676db6d74
pranay0911 <pranaygaikwad1432@gmail.com>
2023-12-09 16:41:12 +0530

    Functions

---
+++ 0-100/Week-1/Callbacks/Functions.js
@@ -0,0 +1,23 @@
+// suppose to take an input
+// run few line of code
+// return the output
+
+
+
+function findSum(n){
+    // function body
+    let ans=0
+    for(let i=1;i<=n;i++){
+        ans=ans+i
+    }
+    return ans;
+}
+// calling function
+console.log(findSum(7));
+
+function sum(a,b){
+    return a+b;
+}
+let finalSum=sum(4,5);
+console.log(finalSum);
+
